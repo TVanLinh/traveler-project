b5ca8b3204f2e5864eb92e2a095f443ea08ff5f4
linhtran <linhtran180895@gmail.com>
2017-07-26 09:04:05 +0700

    config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SlideBarComponent } from './components/slide-bar/slide-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HeaderComponent,
+    FooterComponent,
+    SlideBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    function toggleMenu() {
+        $('.toggle-menu').on('click',function () {
+            $('.menu-tab').toggleClass('tf-0');
+        });
+    }
+
+    $(document).ready(function () {
+        toggleMenu();
+    });
+
+})(jQuery);
